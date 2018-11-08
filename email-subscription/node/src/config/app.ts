b9c600a254796aef59bf44f2aa271d00bc98e5ba
Shankar <shankar21mail@gmail.com>
2018-11-08 14:27:45 +0530

    Updated app config to use actual list id

--- email-subscription/node/src/config/app.ts
+++ email-subscription/node/src/config/app.ts
@@ -1,6 +1,6 @@
 const whiteListedDomains= {
-	'https://safenetwork.tech': '48a0331971',
-	'https://deploy-preview-115--keen-stonebraker-bb1a10.netlify.com/': '48a0331971',
+	'https://safenetwork.tech': '61ce397f57',
+	'https://deploy-preview-115--keen-stonebraker-bb1a10.netlify.com/': '61ce397f57',
 };
 
 export default {
